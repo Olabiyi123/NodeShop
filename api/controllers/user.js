267ed986fa0423403ea3eba770c0c328af01f28e
Olabiyi123 <102982954+Olabiyi123@users.noreply.github.com>
2024-04-23 13:42:15 -0400

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt.js');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
